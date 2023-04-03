36bf1e49e0fb9f4e9cd56e7a71925253bc223a5f
Murtaza S Vhora <Murtazavhora@gmail.com>
2023-04-03 18:38:45 +0530

    Added FailureListener to resolve app crashes when json file is not found or malformed.

--- app/src/main/java/com/example/lottieandroidissue2271/MainActivity.kt
+++ app/src/main/java/com/example/lottieandroidissue2271/MainActivity.kt
@@ -2,10 +2,38 @@ package com.example.lottieandroidissue2271
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.airbnb.lottie.LottieListener
+import com.example.lottieandroidissue2271.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), LottieListener<Throwable> {
+    private var _binding: ActivityMainBinding? = null
+    private val binding get() = _binding!!
+
+    companion object {
+        const val TAG = "MainActivity"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initViews()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
+    private fun initViews() {
+        binding.validJsonUrl.setFailureListener(this)
+        binding.validJsonWithPlaceholder.setFailureListener(this)
+        binding.invalidJsonUrl.setFailureListener(this)
+        binding.invalidJsonWithPlaceholder.setFailureListener(this)
+    }
+
+    override fun onResult(result: Throwable?) {
+        Log.e(TAG, "onResult: ", result)
     }
 }
